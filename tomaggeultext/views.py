b9f2db61ffc3ff56856c696d3f6527449ad58200
이재영 <dldustn14@gmail.com>
2020-09-19 04:06:34 +0900

    메인 페이지 토막글 템플릿 반복

--- tomaggeultext/views.py
+++ tomaggeultext/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import TMText
+from .models import Genre
 
 # Create your views here.
 def tmtext(request):
     all_tmtext = TMText.objects.all()
-    return render(request, 'mainpage.html', {'all_tmtext':all_tmtext},)
+    all_genre = Genre.objects.all()
+    return render(request, 'mainpage.html', {'all_tmtext':all_tmtext, 'all_genre':all_genre})
