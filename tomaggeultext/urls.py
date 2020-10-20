bfcad1906212f8e5c8c61e17dfb763dfe272fc83
baexxbin <soo3886@gmail.com>
2020-10-20 17:43:34 +0900

    토막글 생성createText 대략적인 html

--- tomaggeultext/urls.py
+++ tomaggeultext/urls.py
@@ -3,4 +3,5 @@ from tomaggeultext import views
 
 urlpatterns = [
     path('', views.tmtext, name='main'),
+    path('createText/', views.createText, name="createText"),
 ]
--- tomaggeultext/views.py
+++ tomaggeultext/views.py
@@ -7,3 +7,6 @@ def tmtext(request):
     all_tmtext = TMText.objects.all()
     all_genre = Genre.objects.all()
     return render(request, 'mainpage.html', {'all_tmtext':all_tmtext, 'all_genre':all_genre})
+
+def createText(request):
+    return render(request, 'createText.html')
