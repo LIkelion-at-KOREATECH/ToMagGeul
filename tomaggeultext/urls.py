d2c79fd7651356002190ade418280e58e2f07c66
이재영 <dldustn14@gmail.com>
2020-10-20 16:12:59 +0900

    Update: 메인페이지 url, css

--- tomaggeultext/urls.py
+++ tomaggeultext/urls.py
@@ -3,4 +3,6 @@ from tomaggeultext import views
 
 urlpatterns = [
     path('', views.tmtext, name='main'),
+    path('it_sounds_good/<int:tmt_id>',views.it_sounds_good, name='it_sounds_good'),
+    path('tmtext_detail/<int:tmt_id>',views.tmtext_detail, name='tmtext_detail'),
 ]
--- tomaggeultext/views.py
+++ tomaggeultext/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import TMText
 from .models import Genre
+#from django.http import HttpResponse
+#import json
 
 # Create your views here.
 def tmtext(request):
     all_tmtext = TMText.objects.all()
     all_genre = Genre.objects.all()
     return render(request, 'mainpage.html', {'all_tmtext':all_tmtext, 'all_genre':all_genre})
+
+
+def it_sounds_good(request,tmt_id): # test
+    all_tmtext = TMText.objects.all()
+    all_genre = Genre.objects.all()
+    return render(request, 'mainpage.html', {'all_tmtext':all_tmtext, 'all_genre':all_genre})
+    #context = {'likes_count' : '213', 'message' : '321'}
+    #return HttpResponse(json.dumps(context), content_type='application/json')
+
+def tmtext_detail(request, tmt_id):
+    tmtext=TMText.objects.filter(text_id=tmt_id)
+    return render(request, 'tomaggeul_detail.html', {'tmtext':tmtext})
