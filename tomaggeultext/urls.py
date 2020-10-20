73006622fe81cc5660f931428538c10625cc78c7
Woo-Yeol <king199276@gmail.com>
2020-10-20 17:42:56 +0900

    자바스크립트로 글 생성 부분적으로 감추기 기능

--- tomaggeultext/urls.py
+++ tomaggeultext/urls.py
@@ -3,4 +3,5 @@ from tomaggeultext import views
 
 urlpatterns = [
     path('', views.tmtext, name='main'),
+    path('create/', views.tmtextcreate, name='create'),
 ]
--- tomaggeultext/views.py
+++ tomaggeultext/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import TMText
 from .models import Genre
+from .models import TMSeries
 
 # Create your views here.
 def tmtext(request):
     all_tmtext = TMText.objects.all()
     all_genre = Genre.objects.all()
     return render(request, 'mainpage.html', {'all_tmtext':all_tmtext, 'all_genre':all_genre})
+
+def tmtextcreate(request):
+    all_tmseries = TMSeries.objects.all()
+    return render(request, 'createText.html', {'all_tmseries':all_tmseries})
