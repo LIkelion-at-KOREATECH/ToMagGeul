02957c70de07eb74c8c3a8cc93ff448e2ab2253c
Kimhagyeong <abcd358583@gmail.com>
2020-10-24 16:30:05 +0900

    navbar 살짝 수정

--- tomaggeultext/urls.py
+++ tomaggeultext/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('series/<int:pk>', views.tmlist, name='tmlist'),
     path('it_sounds_good/<int:tmt_id>',views.it_sounds_good, name='it_sounds_good'),
     path('tmtext/<int:tmt_id>',views.tmtext_detail, name='tmtext_detail'),
+    path('popup',views.popup, name='popup'),
 ]
