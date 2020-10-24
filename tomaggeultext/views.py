438177af42a292bedcfee2b3450ddc4bf2a360c3
Woo-Yeol <king199276@gmail.com>
2020-10-24 15:13:04 +0900

    글 생성 Form 생성1

--- tomaggeultext/views.py
+++ tomaggeultext/views.py
@@ -11,14 +11,17 @@ def tmtext(request):
     return render(request, 'mainpage.html', {'all_tmtext':all_tmtext, 'all_genre':all_genre})
 
 def tmtextcreate(request):
+    # writer = request.GET.get.('author', '')
     all_tmseries = TMSeries.objects.values_list()
-    series_form = TMtextCreationForm()
+    tmtext_form = TMtextCreationForm()
     if request.method == "POST":
-        series_form = TMtextCreationForm(request.POST)
-        if series_form.is_valid():
-            series_form.save()
+        tmtext_form = TMtextCreationForm(request.POST)
+        if tmtext_form.is_valid():
+            tmtext = tmtext_form.save(commit=False)
+            tmtext.writer = writer
+            tmtext_form.save()
             return redirect('tomaggeullist')
-    return render(request, 'createText.html', {'all_tmseries':all_tmseries})
+    return render(request, 'createText.html', {'all_tmseries':all_tmseries ,'tmtext_form':tmtext_form})
     
 def tmseriescreate(request):
     # if request.method == "POST":
